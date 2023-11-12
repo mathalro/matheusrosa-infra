e8eca2d50d52ab2bbdf623324b45e94c2d0b9d9b
Matheus Rosa <matheus.ar11@hotmail.com>
2023-11-12 20:04:26 +0000

    Changing user data to install docker

--- cdk/lib/user-data.sh
+++ cdk/lib/user-data.sh
@@ -11,3 +11,9 @@ find /usr/share/nginx/html -type d -exec chmod 2775 {} \;
 find /usr/share/nginx/html -type f -exec chmod 0664 {} \;
 
 echo "<h1>Website Currently in development</h1>" > /usr/share/nginx/html/index.html
+
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
