f4260939123df947f0bcc89ca5f8c07a2062ece7
Matheus Rosa <matheus.ar11@hotmail.com>
2023-11-12 22:54:41 +0000

    Updating user-data.sh with usermod

--- cdk/lib/user-data.sh
+++ cdk/lib/user-data.sh
@@ -17,3 +17,4 @@ sudo yum install -y docker
 sudo service docker start
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -a -G docker ec2-user
